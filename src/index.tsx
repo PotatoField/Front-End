2e9c32eadb4bc0e55680d4311aeedbe58ef9c431
Ji-yoon1030 <keskoe0613@gmail.com>
2024-07-29 15:55:11 +0900

    feat: Add gender page
    젠더 페이지 기본 틀 추가, 필요 없는 폴더 삭제

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FrontGender from './FrontGender';
+
+ReactDOM.render(
+    <React.StrictMode>
+      <FrontGender />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
