7028b037b6a1611f0b4d2b5e97acbb16756f75c5
Xeohn <165402279+xeohyun@users.noreply.github.com>
2024-08-09 14:18:16 +0900

    Save local changes to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import FrontGender from './genderpage/FrontGender';
 
 ReactDOM.render(
     <React.StrictMode>
       <FrontGender />
+    </React.StrictMode>,
+    document.getElementById('root')
+);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../src/mainpage/App';
+
+console.log('Index file loaded');
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
