625ae88d210e4a16f7073d4964bceb5f2ee00dfd
Xeohn <165402279+xeohyun@users.noreply.github.com>
2024-08-12 16:46:41 +0900

    fix mainpage 구축완료

--- src/mainpage/components/Header.js
+++ src/mainpage/components/Header.js
@@ -1,57 +1,60 @@
 import React from 'react';
-import '../front_main.css';
+import Main from '../main.module.css';
+import { FaSearch, FaUser, FaHeart, FaShoppingCart } from "react-icons/fa";
 import {Link, useNavigate} from 'react-router-dom';
-import {FaSearch, FaUser, FaBookmark, FaShoppingCart} from "react-icons/fa";
+
 
 const Header = () => {
   const navigate = useNavigate();
-
   const handleClick = () => {
     navigate('/');
   };
   return (
       <>
-        <header className="top_banner">
-          쇼핑몰 새로운 소식~~~~
+        <header>
+          <div className={Main.top_banner}>
+            쇼핑몰 새로운 소식~~~~
+          </div>
+          <div className={Main.box}>
+            <button className={Main.logoButton} onClick={handleClick}>
+              <p className={Main.logo}>로고</p>
+            </button>
+            <nav className={Main.menuContainer}>
+              <ul className={Main.menu}>
+                <li>
+                  <a href="#gender">Gender</a>
+                  <ul className={Main.submenu}>
+                    <li><a href="#men">Men</a></li>
+                    <li><a href="#women">Women</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#place">Place</a>
+                  <ul className={Main.submenu}>
+                    <li><a href="#date">와글와글</a></li>
+                    <li><a href="#campus">붕어방</a></li>
+                    <li><a href="#vacation">미래관</a></li>
+                    <li><a href="#office">다빈치관</a></li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#info">Info</a>
+                  <ul className={Main.submenu}>
+                    <li><a href="#q&a">Q&A</a></li>
+                    <li><a href="#review">Review</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <div className={Main.icons}>
+                <Link to="/search"><FaSearch className={Main.icons}/></Link>
+                <Link to="/create"><FaUser className={Main.icons}/></Link>
+                <Link to="/bookmark"><FaHeart className={Main.icons}/></Link>
+                <a href="#cart"><FaShoppingCart className={Main.icons}/></a>
+              </div>
+            </nav>
+          </div>
         </header>
-        <header className="box">
-          <button className="logo-button" onClick={handleClick}>
-            <p className="logo">로고</p>
-          </button>
-          <nav className="menu-container">
-            <ul className="menu">
-              <li>
-                <a href="#gender">Gender</a>
-                <ul className="submenu">
-                  <li><a href="#men">Men</a></li>
-                  <li><a href="#women">Women</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#place">Place</a>
-                <ul className="submenu">
-                  <li><a href="#date">와글와글</a></li>
-                  <li><a href="#campus">붕어방</a></li>
-                  <li><a href="#vacation">미래관</a></li>
-                  <li><a href="#office">다빈치관</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="#info">Info</a>
-                <ul className="submenu">
-                  <li><a href="#q&a">Q&A</a></li>
-                  <li><a href="#review">Review</a></li>
-                </ul>
-              </li>
-            </ul>
-            <div className="icons">
-              <Link to="/search"><FaSearch className="icon"/></Link>
-              <Link to="/create"><FaUser className="icon"/></Link>
-              <Link to="/bookmark"><FaBookmark className="icon"/></Link>
-              <a href="#cart"><FaShoppingCart className="icon"/></a>
-            </div>
-          </nav>
-        </header>
+
       </>
   );
 };
